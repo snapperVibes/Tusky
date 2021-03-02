d07cc11130f55488644d108ad986d8e9d5cc4027
Snapper <LearningWithSnapper@gmail.com>
2021-03-01 22:57:12 -0500

    alembic_first_tables

--- app/db.py
+++ app/db.py
@@ -1,5 +1,6 @@
 import os
 
+from sqlalchemy import MetaData
 from sqlalchemy.future import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
@@ -17,4 +18,17 @@ def get_uri() -> str:
 uri = get_uri()
 engine = create_engine(uri)
 SessionalLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# metadata = MetaData(
+#     naming_convention={
+#         "ix": "ix__%(column_0_label)s",
+#         "uq": "uq__%(table_name)s__%(column_0_name)s",
+#         "ck": "ck__%(table_name)s__%(column_0_name)s",
+#         "fk": "fk__%(table_name)s__%(column_0_name)s__%(reffered_table_name)s",
+#         "pk": "pk__%(table_name)s",
+#     }
+# )
+
+# Base = declarative_base(metadata=metadata)
+
 Base = declarative_base()
