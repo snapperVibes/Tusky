67c3c3a7df56402d8a759e6025485947565d9b74
Snapper <LearningWithSnapper@gmail.com>
2021-04-09 23:05:04 -0400

    WIP

--- README.md
+++ README.md
@@ -1,28 +1,7 @@
 # Tusky
 ###### Real-time quiz application
+(Work in progress)
 
 ## Code Style
 ### Tools
 Tusky is auto-formatted using [Black](https://github.com/psf/black).
-
-[comment]: <> (Todo: Add Flake8 to tools)
-
-### Snapper's Style
-Definitions for `__all__` should be at the top of a module, not the bottom.
-This way module exports are able to be found easily without searching.
-
-"Tall" modules are preferred over "wide" packages.
-Although a verbose file structure becomes a necessity for larger projects,
-I (as the developer) just prefer less files.
-
-[Errors are values](https://go-proverbs.github.io/), and exceptions should be exceptional.
-Sadly, Python was written before this mantra took to the mainstream.
-However, I fully believe that Python wouldn't have catchable exceptions if it were written today.
-After all, [explicit is better than implicit](https://www.python.org/dev/peps/pep-0020/#id2).
-
-### Notes and Comments:
-FastAPI's creator, [Sebastián Ramírez](https://github.com/tiangolo), has done a wonderful job making his framework 
-fun and easy to develop for.
-This project's structure is heavily inspired by the [FastAPI/Postgres cookecutter](https://github.com/tiangolo/full-stack-fastapi-postgresql/tree/master/%7B%7Bcookiecutter.project_slug%7D%7D).
-
-The decision to use Vue for the frontend was based solely on the cookiecutter. The initial plan was to do the whole project using FastAPI, 
--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -31,23 +31,31 @@ def create_all(**kw):
     # Add the example quiz
     QUIZ_NAME = "Example Quiz"
     OWNER_NAME = "ADMIN#0000"
-    quiz = schemas.QuizCreate(name=QUIZ_NAME, owner=OWNER_NAME)
+    questions_and_answers = [
+        (
+            "What is your name?",
+            ["Sir Lancelot", "Sir Robin", "Sir Galahad", "King Arthur"],
+        ),
+        ("What is your quest?", ["To seek the Holy Grail"]),
+        ("What is your favorite color?", ["Blue", "Yellow"]),
+    ]
+    questions_inits = []
+    for q_and_a in questions_and_answers:
+        question, answers = q_and_a
+        questions_inits.append(
+            schemas.QuestionCreate(
+                query=question, answers=[schemas.AnswerCreate(text=a) for a in answers]
+            )
+        )
+    quiz = schemas.QuizCreate(
+        quiz_name=QUIZ_NAME, owner_name=OWNER_NAME, questions=questions_inits
+    )
     try:
         crud.quiz.create(db, obj_init=quiz)
     except PendingRollbackError:
         # Todo: Figure out more about this error and if there's a better error to catch
+        #  Currently, this code only works on database initialization
         return
-    # Add questions to quiz
-    queries = [
-        "What is your name?",
-        "What is your quest?",
-        "What is your favorite color?",
-    ]
-    for query in queries:
-       q = schemas.QuestionCreate(
-            query=query, quiz_name=QUIZ_NAME, owner_name=OWNER_NAME
-       )
-       crud.question.create(db, obj_init=q)
     print("YeeHaw 🐴")
 
 
